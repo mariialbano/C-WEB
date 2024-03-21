d7f2eec5037830f1ce9b9cce7a6aaa7d249aa633
mariialbano <marianatalbano@gmail.com>
2024-03-21 19:40:36 -0300

    Adicionando Controllers Contatos e Privacidade

---
+++ MeuProjetoEmMVC/MeuProjetoEmMVC/Controllers/PrivacyController.cs
@@ -0,0 +1,19 @@
+﻿using MeuProjetoEmMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuProjetoEmMVC.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
